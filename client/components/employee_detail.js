576d9210562d41732e999b95d9f2a7fb1af282cc
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-23 20:57:58 +0200

    output all records

--- client/components/employee_detail.js
+++ client/components/employee_detail.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const EmployeeDetail = () => {
+const EmployeeDetail = ({ employee }) => {
+
+    const { name, email, phone, avatar } = employee;
+
     return (
-        <div>
-            Employee Detal!
+        <div className="thumbnail">
+            <img src={avatar} alt={avatar}/>
+            <div className="caption">
+                <h3>{name}</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">Email: {email}</li>
+                    <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+            </div>
         </div>
     )
 };
