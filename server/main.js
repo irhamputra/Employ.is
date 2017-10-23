c5e1a3f5829f125e0fefe9063406c98a89059793
irhamputra <irhamputraprasetyo@gmail.com>
2017-10-23 20:02:34 +0200

    Initial commit

---
+++ server/main.js
@@ -0,0 +1,24 @@
+import _ from "lodash";
+import {Meteor} from "meteor/meteor";
+import {Employees} from "../imports/collections/employees";
+import {image, helpers} from "faker";
+
+Meteor.startup(() => {
+    // generate data
+
+    // check data if exists
+    // see if collections has any records
+    const numberRecords = Employees.find({}).count();
+    if (!numberRecords) {
+        // generate some data
+        _.times(5000, () => {
+            const {name, email, phone} = helpers.createCard();
+            Employees.insert({
+                name,
+                email,
+                phone,
+                avatar: image.avatar()
+            })
+        })
+    }
+});
